8cbf1ca8fbfc0ba98adb342e583740cbf1bddd4b
boyroywax <jmsdevln@gmail.com>
2024-02-13 15:02:26 -0800

    moonbase:setup:apiserver created and working

---
+++ git_commands.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Git commands for monorepo
+
+# Push all changes to remote
+git push https://github.com/d3conomy/airlock airlock:setup
+git push https://github.com/d3conomy/moonbase moonbase:setup
