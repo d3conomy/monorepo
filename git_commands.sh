9f0a79465d43fd5a66c9c0a833769ee8b3d2c118
boyroywax <jmsdevln@gmail.com>
2024-03-13 18:19:34 -0700

    moonbase: remoed status and replaced with log, needs completed, added docs strings

--- git_commands.sh
+++ git_commands.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Git commands for monorepo
+git subtree split --prefix=airlock --branch airlock
+git subtree split --prefix=moonbase --branch moonbase
 
 # Push all changes to remote
 git push https://github.com/d3conomy/airlock airlock:setup
