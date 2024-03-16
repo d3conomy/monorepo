a46ed2e9e21ef3e589118265acb49be28f7e2a81
boyroywax <jmsdevln@gmail.com>
2024-03-15 17:20:02 -0700

    docs: minor cleanup

--- typedoc.sh
+++ typedoc.sh
@@ -6,17 +6,20 @@ npx typedoc \
     --plugin typedoc-plugin-inline-sources \
     --json docs-json/moonbase.json \
     --options moonbase/typedoc.json \
+    --searchInComments \
     --validation.invalidLink false
 
 npx typedoc \
     --plugin typedoc-plugin-inline-sources \
     --json docs-json/airlock-react.json \
     --options airlock/airlock-react/typedoc.json \
+    --searchInComments \
     --validation.invalidLink false
 
 npx typedoc \
     --gitRemote github.com/d3conomy/monorepo.git \
     --gitRevision main \
+    --searchInComments \
     --plugin typedoc-plugin-inline-sources \
     --cleanOutputDir \
     --entryPointStrategy merge "docs-json/*.json"
