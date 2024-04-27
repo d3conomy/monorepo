2072a1ca35ae6519248d103122b75c19e8dfd489
boyroywax <jmsdevln@gmail.com>
2024-04-26 17:03:41 -0700

    artifacts:process-orbitdb-open: begin implementation with process-interface

---
+++ artifacts-ts/src/process-orbitdb-open/commands.ts
@@ -0,0 +1,210 @@
+
+
+import {
+    Database,
+    OrbitDBAccessController,
+    OrbitDb
+} from '@orbitdb/core';
+
+
+import path from 'path';
+import fs from 'fs/promises';
+import { IProcessCommand, IProcessCommandArgInput, ProcessType } from '../process-interface/index.js';
+import { OpenDbProcess } from './process.js';
+
+
+const openDbCommands: Array<IProcessCommand> = [
+    {
+        name: "open",
+        description: "Open a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof OrbitDb
+        ): Promise<typeof Database> => {
+            return await instance.open(
+                args[0].value,
+                args[1].value,
+                args[2].value
+            );
+        },
+        args: [
+            {
+                name: "databaseName",
+                description: "The name of the database to open",
+                required: true
+            },
+            {
+                name: "databaseType",
+                description: "The type of the database to open",
+                required: true
+            },
+            {
+                name: "options",
+                description: "The options to use when opening the database",
+                required: false
+            }
+        ]
+    },
+    {
+        name: "close",
+        description: "Close a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<void> => {
+            return await instance.close();
+        },
+        args: [
+            {
+                name: "databaseName",
+                description: "The name of the database to close",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "address",
+        description: "Get the address of a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<string> => {
+            return instance.address;
+        },
+        args: []
+    },
+    {
+        name: "add",
+        description: "Add data to a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<string> => {
+            return await instance.add(args[0].value);
+        },
+        args: [
+            {
+                name: "data",
+                description: "The data to add to the database",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "all",
+        description: "Get all data from a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<any> => {
+            return await instance.all();
+        },
+        args: []
+    },
+    {
+        name: "get",
+        description: "Get data from a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<any> => {
+            return await instance.get(args[0].value);
+        },
+        args: [
+            {
+                name: "hash",
+                description: "The hash of the data to get from the database",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "put",
+        description: "Put data into a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<string> => {
+            return await instance.put(args[0].value, args[1].value);
+        },
+        args: [
+            {
+                name: "key",
+                description: "The key to use when putting data into the database",
+                required: true
+            },
+            {
+                name: "value",
+                description: "The value to put into the database",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "putDoc",
+        description: "Put a document into a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<string> => {
+            return await instance.putDoc(args[0].value);
+        },
+        args: [
+            {
+                name: "doc",
+                description: "The document to put into the database",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "del",
+        description: "Delete data from a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<void> => {
+            return await instance.del(args[0].value);
+        },
+        args: [
+            {
+                name: "key",
+                description: "The key of the data to delete from the database",
+                required: true
+            }
+        ]
+    },
+    {
+        name: "query",
+        description: "Query a database",
+        type: ProcessType.OPEN_DB,
+        action: async (
+            args: IProcessCommandArgInput[] = new Array<IProcessCommandArgInput>,
+            instance: typeof Database
+        ): Promise<any> => {
+            return await instance.query(args[0].value);
+        },
+        args: [
+            {
+                name: "mapper",
+                description: "The mapper function to use when querying the database",
+                required: true
+            }
+        ]
+    }
+];
+
+
+
+export {
+    openDbCommands
+};
---
+++ artifacts-ts/src/process-orbitdb-open/options.ts
@@ -0,0 +1,64 @@
+
+import { PodProcessId } from '../id-reference-factory/IdReferenceClasses.js';
+import { OrbitDbProcess } from '../process-orbitdb/process.js';
+
+/**
+ * The Types of OrbitDb databases.
+ * @category Database
+ */
+enum OrbitDbTypes {
+    EVENTS = 'events',
+    DOCUMENTS = 'documents',
+    KEYVALUE = 'keyvalue',
+    KEYVALUEINDEXED = 'keyvalueindexed'
+}
+
+const isOrbitDbType = (value: string): OrbitDbTypes => {
+    if(Object.values(OrbitDbTypes).includes(value as OrbitDbTypes)) {
+        return value as OrbitDbTypes;
+    }
+    throw new Error('Invalid OrbitDb type');
+}
+
+/**
+ * The options for opening a database.
+ * @category Database
+ */
+class OpenDbOptions {
+    public id: PodProcessId;
+    public orbitDb: OrbitDbProcess;
+    public databaseName: string;
+    public databaseType: OrbitDbTypes;
+    public dbOptions?: Map<string, string>;
+
+    /**
+     * Constructs a new instance of the _OpenDbOptions class.
+     */
+    constructor({
+        id,
+        orbitDb,
+        databaseName,
+        databaseType,
+        options,
+    }: {
+        id: PodProcessId,
+        orbitDb: OrbitDbProcess,
+        databaseName: string,
+        databaseType?: OrbitDbTypes | string,
+        options?: Map<string, string>
+    }) {
+        this.id = id;
+        this.orbitDb = orbitDb;
+        this.databaseName = databaseName;
+        this.databaseType = databaseType ? isOrbitDbType(databaseType) : OrbitDbTypes.EVENTS;
+        this.dbOptions = options ? options : new Map<string, string>();
+    }
+}
+
+
+export {
+    OrbitDbTypes,
+    isOrbitDbType,
+    OpenDbOptions
+}
+
